530af3b5d80628f70dae399a57996dd5d53fa254
Kevin Macale <kevin.macale@betterask.erni>
2021-10-04 20:27:39 +0800

    Add DTN Initial prject

---
+++ DTN.AssessmentExam/DTN.AssessmentExam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DTN.AssessmentExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
